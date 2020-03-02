bbd0225effbccd062017f642ce088e0a79ee187b
InfernapeXavier <itsgood37@ymail.com>
2020-03-01 22:10:54 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,7 @@ from sentry_sdk.integrations.flask import FlaskIntegration
 
 sentry_sdk.init(
     dsn="https://47f7844dd9d34a1d9b28d4dc906dea58@sentry.io/3394112",
-    integrations=[FlaskIntegration()]
-)
+    integrations=[FlaskIntegration()])
 
 app = Flask(__name__)
 
