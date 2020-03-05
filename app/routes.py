5e959696ce435f5211af07b221a12ffed4914a76
InfernapeXavier <itsgood37@ymail.com>
2020-03-04 22:25:31 -0500

    Added Results
    - Navigation Steps work on Tests

--- app/routes.py
+++ app/routes.py
@@ -4,17 +4,34 @@ import requests
 import json
 
 
-@app.route("/")
-@app.route("/index/")
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/index/", methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        session['testNo'] = 1
+        testNo = session.get('testNo', None)
+        return redirect(url_for('tests', tNo=testNo))
     return render_template("index.html", title='Home')
 
 
-@app.route("/tests/", methods=['GET', 'POST'])
-def tests():
+@app.route("/tests/<tNo>", methods=['GET', 'POST'])
+def tests(tNo):
+    if request.method == 'POST':
+        testNo = session.get('testNo', None)
+        testNo += 1
+        session['testNo'] = testNo
+        if testNo < 5:
+            return redirect(url_for('tests', tNo=testNo))
+        else:
+            return redirect(url_for('results'))
     age = request.form.get('ageGroup')
     print(age)
-    return render_template('tests.html', age=age)
+    return render_template('tests.html', age=age, tNo=tNo)
+
+
+@app.route("/results/")
+def results():
+    return render_template('results.html')
 
 
 @app.route('/debug-sentry/')
