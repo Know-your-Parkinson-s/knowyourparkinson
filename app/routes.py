278b92c46aab30d147a5b54e8ca4a1909f6a7bff
Restyled.io <commits@restyled.io>
2020-03-11 22:08:20 +0000

    Restyled by black

--- app/routes.py
+++ app/routes.py
@@ -5,42 +5,42 @@ import json
 from device_detector import SoftwareDetector
 
 
-@app.route("/", methods=['GET', 'POST'])
-@app.route("/index/", methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        session['testNo'] = 1
-        testNo = session.get('testNo', None)
-        return redirect(url_for('tests', tNo=testNo))
-    return render_template("index.html", title='Home')
+    if request.method == "POST":
+        session["testNo"] = 1
+        testNo = session.get("testNo", None)
+        return redirect(url_for("tests", tNo=testNo))
+    return render_template("index.html", title="Home")
 
 
-@app.route("/tests/<tNo>", methods=['GET', 'POST'])
+@app.route("/tests/<tNo>", methods=["GET", "POST"])
 def tests(tNo):
-    if request.method == 'POST':
-        testNo = session.get('testNo', None)
+    if request.method == "POST":
+        testNo = session.get("testNo", None)
         testNo += 1
-        session['testNo'] = testNo
+        session["testNo"] = testNo
         if testNo < 5:
-            return redirect(url_for('tests', tNo=testNo))
+            return redirect(url_for("tests", tNo=testNo))
         else:
-            return redirect(url_for('results'))
-    age = request.form.get('ageGroup')
+            return redirect(url_for("results"))
+    age = request.form.get("ageGroup")
     print(age)
-    return render_template('tests.html', age=age, tNo=tNo)
+    return render_template("tests.html", age=age, tNo=tNo)
 
 
 @app.route("/results/")
 def results():
-    return render_template('results.html')
+    return render_template("results.html")
 
 
 @app.route("/reaction/")
 def reaction():
-    ua = request.headers.get('User-Agent')
+    ua = request.headers.get("User-Agent")
     device = SoftwareDetector(ua).parse()
-    return render_template('reaction.html', ua=device.os_name())
+    return render_template("reaction.html", ua=device.os_name())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
