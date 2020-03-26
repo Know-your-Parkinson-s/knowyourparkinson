10055e2028a47b8ae59a1deafcfd376cb13556aa
InfernapeXavier <itsgood37@ymail.com>
2020-03-26 16:46:55 -0400

    Added test Page

--- app/static/javascript/test.js
+++ app/static/javascript/test.js
@@ -1,72 +1,42 @@
-if ("LinearAccelerationSensor" in window && "Gyroscope" in window) {
-  document.getElementById("moApi").innerHTML = "Generic Sensor API";
+var el = document.getElementById("sketchpad");
+var pad = new Sketchpad(el);
 
-  let lastReadingTimestamp;
-  let accelerometer = new LinearAccelerationSensor();
-  accelerometer.addEventListener("reading", e => {
-    if (lastReadingTimestamp) {
-      intervalHandler(
-        Math.round(accelerometer.timestamp - lastReadingTimestamp)
-      );
-    }
-    lastReadingTimestamp = accelerometer.timestamp;
-    accelerationHandler(accelerometer, "moAccel");
-  });
-  accelerometer.start();
-
-  if ("GravitySensor" in window) {
-    let gravity = new GravitySensor();
-    gravity.addEventListener("reading", e =>
-      accelerationHandler(gravity, "moAccelGrav")
-    );
-    gravity.start();
+// setLineColor
+function setLineColor(e) {
+  var color = e.target.value;
+  if (!color.startsWith("#")) {
+    color = "#" + color;
   }
-
-  let gyroscope = new Gyroscope();
-  gyroscope.addEventListener("reading", e =>
-    rotationHandler({
-      alpha: gyroscope.x,
-      beta: gyroscope.y,
-      gamma: gyroscope.z
-    })
-  );
-  gyroscope.start();
-} else if ("DeviceMotionEvent" in window) {
-  document.getElementById("moApi").innerHTML = "Device Motion API";
-
-  var onDeviceMotion = function(eventData) {
-    accelerationHandler(eventData.acceleration, "moAccel");
-    accelerationHandler(eventData.accelerationIncludingGravity, "moAccelGrav");
-    rotationHandler(eventData.rotationRate);
-    intervalHandler(eventData.interval);
-  };
-
-  window.addEventListener("devicemotion", onDeviceMotion, false);
-} else {
-  document.getElementById("moApi").innerHTML =
-    "No Accelerometer & Gyroscope API available";
+  pad.setLineColor(color);
 }
+document.getElementById("line-color-input").oninput = setLineColor;
 
-function accelerationHandler(acceleration, targetId) {
-  var info,
-    xyz = "[X, Y, Z]";
-
-  info = xyz.replace("X", acceleration.x && acceleration.x.toFixed(3));
-  info = info.replace("Y", acceleration.y && acceleration.y.toFixed(3));
-  info = info.replace("Z", acceleration.z && acceleration.z.toFixed(3));
-  document.getElementById(targetId).innerHTML = info;
+// setLineSize
+function setLineSize(e) {
+  var size = e.target.value;
+  pad.setLineSize(size);
 }
+document.getElementById("line-size-input").oninput = setLineSize;
 
-function rotationHandler(rotation) {
-  var info,
-    xyz = "[X, Y, Z]";
-
-  info = xyz.replace("X", rotation.alpha && rotation.alpha.toFixed(3));
-  info = info.replace("Y", rotation.beta && rotation.beta.toFixed(3));
-  info = info.replace("Z", rotation.gamma && rotation.gamma.toFixed(3));
-  document.getElementById("moRotation").innerHTML = info;
+// undo
+function undo() {
+  pad.undo();
 }
+document.getElementById("undo").onclick = undo;
 
-function intervalHandler(interval) {
-  document.getElementById("moInterval").innerHTML = interval;
+// redo
+function redo() {
+  pad.redo();
 }
+document.getElementById("redo").onclick = redo;
+
+// clear
+function clear() {
+  pad.clear();
+}
+document.getElementById("clear").onclick = clear;
+
+// resize
+window.onresize = function(e) {
+  pad.resize(el.offsetWidth);
+};
